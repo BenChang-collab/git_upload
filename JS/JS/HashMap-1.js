b11bc895f02a9b4a64bb3b836659cb57ebea3653
“Ben_00550354” <zxc110009@gmail.com>
2024-08-28 17:25:51 +0800

    備份

---
+++ JS/JS/HashMap-1.js
@@ -0,0 +1,21 @@
+
+var HashMap = function () {
+    let map = {};
+    return {
+        put: function (key, value) {
+            map[key] = value;
+        },
+        keys: function () {
+            return Object.keys(map);
+        },
+        contains: function (key) {
+            return map.hasOwnProperty(key);
+        },
+        get: function (key) {
+            return map[key];
+        },
+        clear: function () {
+            return map = {};
+        }
+    };
+};
